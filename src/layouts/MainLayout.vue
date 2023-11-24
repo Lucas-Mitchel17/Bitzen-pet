3d422644c9fa4a09c06d99cdd00f5b4561d73058
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-11-24 12:01:50 -0300

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" class="top-btn" />
+    </q-page-scroller>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {},
+});
+</script>
+
+<style lang="scss">
+.top-btn {
+  background-color: aqua;
+  color: black;
+}
+</style>
