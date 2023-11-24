cab943fe8fdfd3cb1f67093250f05bf47dba0850
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-11-24 13:56:46 -0300

    feat: Create BaseText Component

---
+++ src/components/ui/base/BaseText/BaseText.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { computed } from "vue";
+import { TAGS_ENUM } from "./types";
+
+const props = defineProps({
+  tag: {
+    type: String,
+    default: "p",
+    validator: (value) => Object.values(TAGS_ENUM).includes(value),
+  },
+});
+
+const classes = computed(() => {
+  const defaultTextClass = `text-${props.tag}`;
+
+  return [defaultTextClass];
+});
+</script>
+
+<template>
+  <component :is="tag" :class="classes" class="text">
+    <slot />
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.text {
+  line-height: 1.15;
+  margin: 0;
+}
+.text-h1 {
+  font-size: 20px;
+  color: $blue;
+  margin: 40px 0 0;
+
+  @media (min-width: $sm) {
+    font-size: 30px;
+  }
+
+  @media (min-width: $md) {
+    font-size: 40px;
+  }
+}
+
+.text-h2 {
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 36px;
+}
+
+.text-h3 {
+  font-size: 30px;
+  margin: 0 0 10px;
+}
+.text-h4 {
+  font-size: 20px;
+  margin: 0 0 10px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,9 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
+import { BaseText } from "src/components/ui/base";
 </script>
+
+<template>
+  <div class="flex column justify-center items-center">
+    <BaseText tag="h2"> Teste de componente </BaseText>
+  </div>
+</template>
